e09d8025db32713da4c3579bc42fc52dbc166ce6
Arslan <muhammad.arslan.82jb@gmail.com>
2024-02-21 21:04:26 +0500

    env setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import Register from './Components/Register';
-import Login from './Components/Login';
-import { initializeApp } from 'firebase/app';
+import React, { Component } from "react";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import { initializeApp } from "firebase/app";
 import {
-  getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,
-  signInWithPopup, GoogleAuthProvider
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
 } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyDSqNCgSK7mtkPYoDA2CaDfz8ubO97CjSU",
-  authDomain: "fir-project-9e7eb.firebaseapp.com",
-  databaseURL: "https://fir-project-9e7eb-default-rtdb.firebaseio.com",
-  projectId: "fir-project-9e7eb",
-  storageBucket: "fir-project-9e7eb.appspot.com",
-  messagingSenderId: "1010234250070",
-  appId: "1:1010234250070:web:c926dd0a2b6ae2330e3cf7"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -26,7 +29,7 @@ export class App extends Component {
       message: "",
       type: 1,
     };
-  };
+  }
   pageSwitchHandler = (event) => {
     this.setState({ page: !this.state.page, message: null });
     event.preventDefault();
@@ -37,9 +40,9 @@ export class App extends Component {
     const password = event.target.password.value;
     const confirmPassword = event.target.confirmPassword.value;
     if (password !== confirmPassword) {
-      this.setState({ message: "Passwords do not match!", type: 0 })
+      this.setState({ message: "Passwords do not match!", type: 0 });
       return;
-    };
+    }
     createUserWithEmailAndPassword(auth, email, password)
       .then((data) => {
         this.setState({ message: "Registered succesfully!", type: 1 }, () => {
@@ -63,41 +66,58 @@ export class App extends Component {
           this.setState({ message: "Login successful", type: 1 }, () => {
             event.target.email.value = "";
             event.target.password.value = "";
-          })
-        else { this.setState({ message: "User not verified!", type: 0 }) }
+          });
+        else {
+          this.setState({ message: "User not verified!", type: 0 });
+        }
       })
       .catch((error) => {
         this.setState({ message: error.message, type: 0 });
       });
-  }
+  };
   googleLoginHandler = (event) => {
     event.preventDefault();
     signInWithPopup(auth, provider)
       .then((data) => {
         if (data._tokenResponse.isNewUser === true)
-          this.setState({ message: `"Login successful ${data._tokenResponse.fullName}"`,  type: 1 })
-        else { this.setState({ message: "Already Login with this Google Account!", type: 0 }) }
-      }).catch((error) => {
+          this.setState({
+            message: `"Login successful ${data._tokenResponse.fullName}"`,
+            type: 1,
+          });
+        else {
+          this.setState({
+            message: "Already Login with this Google Account!",
+            type: 0,
+          });
+        }
+      })
+      .catch((error) => {
         this.setState({ message: error.message, type: 0 });
       });
-  }
+  };
   render() {
     return (
       <div>
-        {this.state.page ? (<Register
-          google={this.googleLoginHandler}
-          type={this.state.type}
-          switch={this.pageSwitchHandler}
-          message={this.state.message}
-          register={this.registrationHandler} />) :
-          (<Login type={this.state.type}
+        {this.state.page ? (
+          <Register
+            google={this.googleLoginHandler}
+            type={this.state.type}
+            switch={this.pageSwitchHandler}
+            message={this.state.message}
+            register={this.registrationHandler}
+          />
+        ) : (
+          <Login
+            type={this.state.type}
             google={this.googleLoginHandler}
             login={this.loginHandler}
             message={this.state.message}
-            switch={this.pageSwitchHandler} />)}
+            switch={this.pageSwitchHandler}
+          />
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
